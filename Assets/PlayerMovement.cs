a0eca5b44c211e487812536fa425de6801bbea46
Akshat Khurana <akshat.khurana@yahoo.com>
2024-07-19 16:07:30 +1000

    Added to the player jumping code and worked on crouching functionality
    I actually learnt a bunch of things while doing this;
    
    - how to use project settings to set keys
    - using the #region tag to make a code block collapsible
    - how Input and its functions can be used to get different stages of a button/key press

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,21 +5,51 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform graphics;
     [SerializeField] private float jumpForce = 10f;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float groundDistance = 0.25f;
     [SerializeField] private Transform feetPos;
     [SerializeField] private float jumpTime = 0.3f;
+    [SerializeField] private float crouchHeight = 0.5f;
 
     private bool isGrounded = false;
+    private bool isJumping = false;
+    private float jumpTimer;
 
     private void Update() {
-        isGrounded = Physics2D.OverlapCircle(feetPos.position,
-         groundDistance, groundLayer);
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, groundDistance, groundLayer);
+
+        #region JUMPING
 
         if (isGrounded && Input.GetButtonDown("Jump")) {
             isJumping = true;
             rb.velocity = Vector2.up * jumpForce;
         }
+
+        if (isJumping && Input.GetButton("Jump")) {
+            if (jumpTimer < jumpTime) {
+                rb.velocity = Vector2.up * jumpForce;
+
+                jumpTimer += Time.deltaTime;
+            } else {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump")) {
+            isJumping = false;
+            jumpTimer = 0;
+        }
+
+        #endregion
+
+        #region CROUCHING
+
+        if (isGrounded && Input.GetButton("Crouch")) {
+            graphics.localScale = new Vector3(graphics.localScale.x, crouchHeight, graphics.localScale.z);
+        }
+
+        #endregion
     }
 }
